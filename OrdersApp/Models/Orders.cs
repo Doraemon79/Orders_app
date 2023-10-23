9c26cfbb366e2f47b6e679974517b1e3be609973
Doraemon79 <ignleotta@gmail.com>
2023-10-23 16:46:00 +0200

    First commit

---
+++ OrdersApp/Models/Orders.cs
@@ -0,0 +1,10 @@
+﻿namespace OrdersApp.Models
+{
+    public class Orders
+    {
+        public Guid Id { get; set; }
+        public string OrderName { get; set; }
+        public double Price { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
